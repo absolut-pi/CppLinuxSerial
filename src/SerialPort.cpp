13492d1fb33770fcc6fdd19fb73df3bfceb7a19a
machinetherapist <79029454+illegal-instruction-co@users.noreply.github.com>
2023-09-18 18:18:14 +0300

    Update SerialPort.cpp

--- src/SerialPort.cpp
+++ src/SerialPort.cpp
@@ -8,7 +8,6 @@
 //!             See README.rst in repo root dir for more info.
 
 // System includes
-#include <termios.h>
 #include <iostream>
 #include <sstream>
 #include <stdio.h>      // Standard input/output definitions
